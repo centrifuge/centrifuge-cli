f160a6de31248a3289e4bd9373ad46ec02efd00d
nuno <hi@nunoalexandre.com>
2022-03-29 14:55:50 +0200

    Drop config variant for whole pallet migration

--- packages/plugins/migration/src/migration/interfaces.ts
+++ packages/plugins/migration/src/migration/interfaces.ts
@@ -10,42 +10,22 @@ export type Migrations = Array<Migration>;
 // A migration, i.e, a type capturing the migratable item in the source and in the destination chain.
 // For example, { source: (RadClaims, AccountBalances), destination: (Claims, ClaimedAmounts) }.
 export interface Migration {
-    // The migratable info on the source chain
-    source: Migratable,
-    // The migratable info on the destination chain
-    destination: Migratable
+    // The source storage item
+    source: StorageItem,
+    // The destination storage item
+    destination: StorageItem
 }
 
-// Union type capturing a migratable item.
-export type Migratable = Pallet | PalletStorageItem;
-
-export interface Pallet {
-    type: MigratableType.Pallet;
-    name: string;
-}
-
-// A Pallet Storage item, e.g. `{ pallet: "Balances", name: "TotalIssuance" }`
-export interface PalletStorageItem {
-    type: MigratableType.PalletStorageItem;
-    // The pallet this item is under
+// A Pallet Storage item, e.g. `{ pallet: "Balances", item: "TotalIssuance" }`
+export interface StorageItem {
+    // The name of the pallet
     pallet: string;
     // The name of the storage item
-    name: string;
+    item: string;
 }
 
-// The different Migratable variant types
-export enum MigratableType {
-    Pallet = "Pallet",
-    PalletStorageItem = "PalletStorageItem",
-}
-
-export function toStorageElement(m: Migratable): StorageElement {
-    switch (m.type) {
-        case MigratableType.Pallet:
-            return new PalletElement(m.name);
-        case MigratableType.PalletStorageItem:
-            return new StorageItemElement(m.pallet, m.name);
-    }
+export function toStorageElement(x: StorageItem): StorageElement {
+    return new StorageItemElement(x.pallet, x.item)
 }
 
 export interface MigrationSummary {
